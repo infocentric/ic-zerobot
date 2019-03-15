bf2b410c100778df8b97ffb0155aba8ece4b4030
Petar <p.angelkovic@hotmail.com>
2019-03-15 11:03:10 +0100

    Initial commit

---
+++ ICZerobot/Models/Task/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ICZerobot.Models.Task
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<byte[]> Attachements { get; set; }
+        public string Priority { get; set; }
+        public string Creator { get; set; }  
+        public string Project { get; set; }
+        public string System { get; set; }
+        public string TaskType { get; set; }
+        public string Estimate { get; set; }
+        public DateTime DueDate { get; set; }
+        public Dictionary<string, string> AdditionalParameters { get; set; }
+
+    }
+
+}
